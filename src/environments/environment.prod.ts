66c8562d18951887f777176f9d49d971c8d5f2da
Lev <33432198+levnoppe@users.noreply.github.com>
2020-06-25 16:33:17 +0300

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseAPIKey: 'AIzaSyBELSaRoTQfEekEsiq3zJN0rI9HTYbtQKg',
-  bingNewsApiKey: '0f78528e9a2b4bd2af46929953cde46d',
-  newsOrgApiKey: '9595a82887484dd4b6793c98580b448b'
-};
